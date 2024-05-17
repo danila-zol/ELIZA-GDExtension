a8d4fcb7ada1cc9f902e165bc5a40d7da2035ab0
Danila Zolotarev <danila-zol@list.ru>
2024-05-17 11:10:35 +0300

    Improved the demo project

--- demo/eliza_input.gd
+++ demo/eliza_input.gd
@@ -8,6 +8,7 @@ var input_start: int
 var user_input: String
 var eliza_answer: String
 var last_line_count: int
+var currently_printing: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,34 +19,38 @@ func _ready() -> void:
 	last_line_count = get_line_count()
 
 
-
-
-func _on_text_changed() -> void:
-	# The user could not delete lines
-	if get_line_count() < last_line_count:
-		undo()
-		set_caret_line(get_line_count() - 1)
-		return
-
-	if text[-1] == '\n':
-		# editable = false
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_text_newline"):
 		# Trim to the actual sentance and remove newline at the end.
 		user_input = text.substr(input_start, -1)
 		if user_input.length() >= 2:
 			user_input[-1] = ""
 
 		eliza_answer = $ELIZA.answer(user_input)
-		await eliza_print(eliza_answer)
+		await eliza_print('\n' +  eliza_answer)
 
 		last_line_count = get_line_count()
 		input_start = text.length()
 
 		set_caret_line(get_line_count() - 1)
 		set_caret_column(0)
-		# editable = true
+
+
+func _on_text_changed() -> void:
+	# The user could not delete lines
+	if get_line_count() < last_line_count:
+		text += '\n'
+		set_caret_line(get_line_count() - 1)
+		return
 
 
 func eliza_print(s: String):
+		# Simple lock against simultaneus calls
+		if currently_printing:
+			return
+		else:
+			currently_printing = true
+
 		editable = false
 		var caret_column = get_caret_column()
 		var t = get_tree()
@@ -60,7 +65,9 @@ func eliza_print(s: String):
 			set_caret_column(caret_column)
 
 		text += "\n\n"
+
 		editable = true
+		currently_printing = false
 
 
 func _on_focus_entered() -> void:
